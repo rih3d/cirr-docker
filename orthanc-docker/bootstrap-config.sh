69e85ff4f014eae5cef19418b721339c0215ade4
Derek <derek_merck@brown.edu>
2016-05-10 15:39:47 -0400

    Fixes on CIRR

--- orthanc-docker/bootstrap-config.sh
+++ orthanc-docker/bootstrap-config.sh
@@ -3,7 +3,7 @@
 export DB_HOST="postgres"
 export DB_NAME="orthanc"
 
-cat orthanc.json.template \
+cat orthanc.template.json \
         | sed "s|%DB_HOST%|${DB_HOST}|" \
         | sed "s|%DB_NAME%|${DB_NAME}|" \
         | sed "s|%DB_USER%|${POSTGRES_CIRR_USER}|" \
