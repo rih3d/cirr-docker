10f4007887e088d824628f43d32a5e656599e087
Derek Merck <Derek_Merck@brown.edu>
2016-05-10 13:40:47 -0400

    Latest orthanc docker, nothing else works

--- orthanc-docker/bootstrap-config.sh
+++ orthanc-docker/bootstrap-config.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export DB_HOST="db"
-export DB_ORTHANC_NAME="orthanc"
+export DB_HOST="postgres"
+export DB_NAME="orthanc"
 
 cat orthanc.json.template \
         | sed "s|%DB_HOST%|${DB_HOST}|" \
-        | sed "s|%DB_ORTHANC_NAME%|${DB_ORTHANC_NAME}|" \
-        | sed "s|%DB_ORTHANC_USER%|${DB_ORTHANC_USER}|" \
-        | sed "s|%DB_ORTHANC_PASSWORD%|${DB_ORTHANC_PASSWORD}|" \
-        > shadow.orthanc.json
+        | sed "s|%DB_NAME%|${DB_NAME}|" \
+        | sed "s|%DB_USER%|${POSTGRES_CIRR_USER}|" \
+        | sed "s|%DB_PASSWORD%|${POSTGRES_CIRR_PASSWORD}|" \
+        > orthanc.shadow.json
